06711a7ea9194c179cb281d2ff8847ea44986b46
Jesus Seijas <jesus.seijas@axa.com>
2021-01-30 12:50:20 +0100

    First Commit

---
+++ wav_to_mp3.py
@@ -0,0 +1,14 @@
+import os
+import glob
+from pydub import AudioSegment
+
+for filename in glob.iglob('./source/**/*.wav', recursive=True):
+  path = os.path.normpath(filename)
+  tokens = path.split(os.sep)
+  print("procesando " + filename)
+  targetpath = os.path.join('./audios/', tokens[2], )
+  target = os.path.join(targetpath, os.path.splitext(tokens[3])[0]+'.mp3')
+  os.makedirs(targetpath, exist_ok = True)
+  sound = AudioSegment.from_wav(filename)
+  sound.export(target, format = 'mp3')
+  print(target)
